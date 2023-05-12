da0318eea0069afcfb89f0a077c79c1d97e35e32
Beelmann, Marko <marko.beelmann@philips.com>
2023-05-12 13:57:54 +0200

    feat: add path of raw json data file as output
    Now it's possible to get the raw json data from the pull request as used
    in the action itself. This can be handy if someone wants to collect this
    data in a different place like a database over multiple repos.
    
    With the path of the raw json data file he can upload it to a central
    database for further processing.

--- src/run.ts
+++ src/run.ts
@@ -2,13 +2,32 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { AddCommentToPR, GetPullRequestData } from './GitHubCliHelper'
 import { ReportGenerator } from './Report.Generation'
-import { Report } from './Report.Definitions'
-import { GetActiveMeasures, MetricTable, UpdateConfigValues } from './Report.Measures'
+import { Report, ReportMeasurementEntry } from './Report.Definitions'
+import { GetActiveMeasures, MetricTable, UpdateConfigValues as UpdateConfig } from './Report.Measures'
 import { PullRequest } from './PullRequest.Definitions'
 import * as fs from 'fs'
 import { ConfigurationInputs } from './action.config.type'
+import { IPullRequest } from './Interfaces/PullRequestTypes'
+import { IReport } from './Interfaces/ReportTypes'
 
-// eslint-disable-next-line @typescript-eslint/require-await
+const CreatePRCommentFile = (prData: unknown, commentText: string): string => {
+  // generate random file name
+  const fileName = Math.random().toString(36).substring(7) + '.md'
+  const jsonString = JSON.stringify(prData)
+  // write report string to file
+  fs.writeFileSync(fileName, `<!-- ${jsonString} -->\n${commentText}`)
+
+  return `${process.env.GITHUB_WORKSPACE || './'}/${fileName}`
+}
+
+const GenerateReport = (activeConfigValues: ReportMeasurementEntry[], pullRequestDataModel: IPullRequest): IReport => {
+  const report = new Report()
+  report.Entries = activeConfigValues
+  report.Description = 'Test report'
+  report.Id = pullRequestDataModel.id.toString()
+  return report
+}
+
 export const run = async (inputsFromWorkflow: ConfigurationInputs): Promise<number> => {
   // take care that action is running only in PR context
   if (process.env.GITHUB_EVENT_NAME !== 'pull_request') {
@@ -16,24 +35,25 @@ export const run = async (inputsFromWorkflow: ConfigurationInputs): Promise<numb
     return 0
   }
 
-  UpdateConfigValues(inputsFromWorkflow, MetricTable)
+  UpdateConfig(inputsFromWorkflow, MetricTable)
   const activeConfigValues = GetActiveMeasures(MetricTable)
 
-  // get PR number
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+  // get PR data from github cli
   const cliPullRequestData = await GetPullRequestData(github.context.issue.number)
+  // transform PR data to a typed model
   const pullRequestDataModel = PullRequest.CreateFromJson(cliPullRequestData)
+  // generate the report of the typed model
   const generator = new ReportGenerator()
-  const report = new Report()
-  report.Entries = activeConfigValues
-  report.Description = 'Test report'
-  report.Id = pullRequestDataModel.id.toString()
+  const report = GenerateReport(activeConfigValues, pullRequestDataModel)
+  // create report
   const reportAsString = generator.Generate(pullRequestDataModel, report)
-  // generate random file name
-  const fileName = Math.random().toString(36).substring(7) + '.md'
-  // write report string to file
-  fs.writeFileSync(fileName, `<!-- ${JSON.stringify(cliPullRequestData)} -->\n${reportAsString}`)
-  //  core.info(`my name is ${inputs.name}`)
-  await AddCommentToPR(fileName, pullRequestDataModel.id)
-  return 12
+
+  const commentPath = CreatePRCommentFile(cliPullRequestData, reportAsString)
+  await AddCommentToPR(commentPath, pullRequestDataModel.id)
+
+  const jsonPath = commentPath.replace(`/\\.md$/`, '.json')
+  fs.writeFileSync(jsonPath, JSON.stringify(cliPullRequestData))
+  core.setOutput('json-report-path', jsonPath)
+
+  return 0
 }
